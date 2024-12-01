04038e3e082c056e33e39018822fd5d6ba655df7
Ariel Spencer <arielspencer.tech@gmail.com>
2024-11-30 21:09:40 -0300

    feat: add functions, arrow functions, and conditionals

--- refresher.js
+++ refresher.js
@@ -10,3 +10,23 @@ let booleano = true;
 let nulo = null;
 let indefinido;
 let objeto = { nome: "Ariel", idade: 28 };
+
+// 3. Funções
+function saudacao(nome) {
+  return `Olá, ${nome}!.`;
+}
+console.log(saudacao("Ariel"));
+
+// Arrow function
+const soma = (a, b) => a + b;
+console.log(`A soma de 3 e 4 é: ${soma(3, 4)}`);
+
+// 4. Condicionais
+let nota = 85;
+if (nota >= 90) {
+  console.log("Nota: A, passou!");
+} else if (nota >= 70) {
+  console.log("Nota: B, passou!");
+} else {
+  console.log("Nota: C, reprovou!");
+}
